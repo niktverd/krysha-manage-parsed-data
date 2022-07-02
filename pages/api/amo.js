bb0c034cf95f89a47727cd9d83a3a8ac482e2e1f
Nik Tverd <niktverd@yandex-team.ru>
2022-07-02 17:26:58 +0600

    fixup! fixup! fixup! first commit

--- pages/api/amo.js
+++ pages/api/amo.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Client } from 'amocrm-js';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
     res.status(200).json({ name: 'John Doe' })
 
     const client = new Client({
