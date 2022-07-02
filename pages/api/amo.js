d88b1a22d4bb8ab6065b2a4ca859842418eb6357
Nik Tverd <niktverd@yandex-team.ru>
2022-07-02 17:17:58 +0600

    fixup! first commit

--- pages/api/amo.js
+++ pages/api/amo.js
@@ -1,27 +1,19 @@
-const { Client } = require('amocrm-js');
-const ngrok = require('ngrok');
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Client } from 'amocrm-js';
 
+export default function handler(req, res) {
+    res.status(200).json({ name: 'John Doe' })
 
-const run = async () => {
-    const port = 3000;
     const client = new Client({
         domain: 'yew.amocrm.ru',
         auth: {
             client_id: '30116053',
             client_secret: 'dYgWLXWod1JNOgI21EI9frWqoS5YiZj0jY0OTPXsCLSc6h7pSVVNJ9GZOOqAfvcn',
-            server: {
-                port
-            }
         }
     });
 
-    console.log('Включаю ngrok...');
-    const url = await ngrok.connect(port);
-    console.log('Укажите адрес в настройках интеграции AmoCRM:', url);
-
     client.environment.set('auth.redirect_uri', url);
 
-    const authUrl = client.auth.getUrl();
     console.log('Перейдите по адресу для завершения авторизации', authUrl);
 
     try {
@@ -31,9 +23,4 @@ const run = async () => {
     catch (e) {
         console.log('Ошибка установления соединения', e);
     }
-
-    console.log('Выключаю ngrok...');
-    await ngrok.kill();
-};
-
-run();
+}
